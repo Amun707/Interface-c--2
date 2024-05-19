7a8c81d333379151e42f0ba0e0474a029e7634f1
Каролина <karollavru@gmail.com>
2024-05-19 13:35:27 +0500

    Добавьте файлы проекта.

---
+++ ISalary.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_c__2
+{
+    interface ISalary
+    {
+        string FullName { get; set; }
+        int WorkingDays { get; set; }
+        double CalculateSalary();
+    }
+    interface IConsoleOutput
+    {
+        void Print();
+    }
+
+    class Manager : ISalary, IConsoleOutput
+    {
+        public string FullName { get; set; }
+        public int WorkingDays { get; set; }
+
+        public double CalculateSalary()
+        {
+            return WorkingDays * 1000;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Manager {FullName}:");
+            Console.WriteLine($"Working days: {WorkingDays}");
+            Console.WriteLine($"Salary: {CalculateSalary()}");
+        }
+    }
+
+    class DepartmentHead : ISalary, IConsoleOutput
+    {
+        public string FullName { get; set; }
+        public int WorkingDays { get; set; }
+
+        public double CalculateSalary()
+        {
+            return WorkingDays * 2500;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Department Head {FullName}:");
+            Console.WriteLine($"Working days: {WorkingDays}");
+            Console.WriteLine($"Salary: {CalculateSalary()}");
+        }
+    }
+
+    class Accountant
+    {
+        public double CalculateIncomeTax(ISalary employee, int days)
+        {
+            double salary = employee.CalculateSalary() * days / employee.WorkingDays;
+            return salary * 0.13;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Interface_c__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager { FullName = "John Doe", WorkingDays = 22 };
+            DepartmentHead head = new DepartmentHead { FullName = "Alice Smith", WorkingDays = 20};
+
+            manager.Print();
+            head.Print();
+
+            Accountant accountant = new Accountant();
+            Console.WriteLine($"Income tax for Manager:{accountant.CalculateIncomeTax(manager, 22)}");
+            Console.WriteLine($"Income tax for Department Head:{accountant.CalculateIncomeTax(head, 20)}");
+        }
+    }
+}
